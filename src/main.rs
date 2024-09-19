2af7d861521f7db336d0179882aa647ca1b708bf
brayckner <brayckner@gmail.com>
2024-09-19 11:36:00 -0400

    Setting up transaction object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use transaction::Transaction;
+
+mod transaction;
+mod tracker;
+mod ui;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Personal Finance Tracker");
+
+    let transaction = Transaction::new(111, 70.85, "Georgia Tech Bookstore".to_string(), "09-19-2024".to_string());
+    transaction.display()
+    
 }
---
+++ src/transaction.rs
@@ -0,0 +1,22 @@
+// Represents a financial transaction with fields like id, amount, category, and date.
+// Methods for creating, displaying, and serializing transactions.
+
+pub struct Transaction {
+    pub amount: f64,
+    pub id: u32,
+    pub category: String,
+    pub date: String,
+}
+
+impl Transaction {
+
+    pub fn new(id: u32, amount: f64, category: String, date: String) -> Self {
+        Self {id, amount, category, date}
+    }
+
+    pub fn display(&self) {
+        println!("ID: {}, Amount: {}, Category: {}, Date: {}", self.id, self.amount, self.category, self.date);
+    }
+    
+}
+
