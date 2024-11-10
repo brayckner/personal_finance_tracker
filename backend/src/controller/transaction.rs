ee3e90122b43e035d713499dccdd605fa4f5eb08
brayckner <brayckner@gmail.com>
2024-11-09 21:44:35 -0600

    Adding function to start HTTP Client and prepare request with headers.

--- backend/src/controller/transaction.rs
+++ backend/src/controller/transaction.rs
@@ -1,4 +1,7 @@
-use actix_web::{ HttpResponse, Responder};
+use std::fmt::Debug;
+use actix_web::{HttpResponse, Responder};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use crate::models::transaction::TransactionInput;
 use crate::services::transaction::{create_transaction, fetch_transactions, remove_transaction};
@@ -22,5 +25,40 @@ pub async fn delete_transaction(data: actix_web::web::Data<AppState>, id: actix_
     return match remove_transaction(&data, *id).await {
         Ok(_) => HttpResponse::Ok().body("Transaction Deleted"),
         Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct TiingoApiResponse {
+    pub message: String,
+}
+pub async fn fetch_financial_data() -> impl Responder {
+
+    // Creating HTTP Client
+    let client = Client::new();
+
+    // API URL and Token
+    let url = "https://api.tiingo.com/api/test/";
+    let auth_token = dotenv::var("TIINGO_API_KEY").expect("TIINGO_API_KEY not set in .env");
+
+    // prepare the request with headers
+    let response = client.get(url)
+        .header("Content-type", "application/json")
+        .header("Authorization", format!("Token {}", auth_token))
+        .send()
+        .await;
+
+    match response {
+        Ok(response) => {
+            if response.status().is_success() {
+                match response.json::<TiingoApiResponse>().await {
+                    Ok(api_response) => HttpResponse::Ok().json(api_response),
+                    Err(_) => HttpResponse::InternalServerError().body("Failed to parse response")
+                }
+            } else {
+                HttpResponse::InternalServerError().body("External API request failed")
+            }
+        }
+        Err(_) => HttpResponse::InternalServerError().body("Failed to send request")
     }
 }
